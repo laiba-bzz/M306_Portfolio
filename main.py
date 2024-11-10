3439ea38d802bf1881d36a5d7216d090180b1207
laiba-bzz <laiba@bzz.ch>
2024-11-10 23:42:35 +0100

    s

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+from functools import reduce
 from flask import Flask, jsonify, request
 
 
@@ -54,6 +56,13 @@ def get_plants(plant, item_id):
 def get_all_plants():
     return jsonify(plants), 200
 
+def get_random_plant(plantso):
+    return random.choice(plantso)
+
+@app.route('/plants/random', methods=['GET'])
+def random_plant():
+    plant = get_random_plant(plants)
+    return jsonify(plant), 200
 
 @app.route('/plants/<int:item_id>', methods=['GET'])
 def find_plant_by_id(item_id):
@@ -76,6 +85,14 @@ def sorted_plants():
     sorted_plants = sorted(plants, key=lambda x: x['title'])
     return jsonify(sorted_plants), 200
 
+@app.route('/plants/sorted_by_word/<string:word>', methods=['GET'])
+def sort_plants_by_word(word):
+    sorted_plants = sorted(
+        plants,
+        key=lambda plant: plant['description'].lower().find(word.lower()) if word.lower() in plant['description'].lower() else float('inf')
+    )
+    return jsonify(sorted_plants), 200
+
 
 @app.route('/plants/filtered', methods=['GET'])
 def filtered_plants():
@@ -87,6 +104,23 @@ def filtered_plants_wort(search_term):
     filtered = tuple(filter(lambda plant: search_term.lower() in plant['description'].lower(), plants))
     return jsonify(filtered), 200
 
+
+@app.route('/plants/combined', methods=['GET'])
+def combined_operations():
+    filtered_plantso = filter(lambda plant: 'licht' in plant['description'].lower(), plants)
+    mapped_titles = map(lambda plant: plant['title'].upper(), filtered_plantso)
+    total_length = reduce(lambda acc, title: acc + len(title), mapped_titles, 0)
+
+    return jsonify({'total_title_length': total_length}), 200
+
+
+@app.route('/plants/aggregate', methods=['GET'])
+def aggregate_data():
+    filtered_plantse = filter(lambda plant: 'licht' in plant['description'].lower(), plants)
+    mapped_titles = map(lambda plant: plant['title'].upper(), filtered_plantse)
+    total_title_length = reduce(lambda acc, title: acc + len(title), mapped_titles, 0)
+
+    return jsonify({'total_title_length': total_title_length}), 200
 
 @app.route('/plants/create', methods=['POST'])
 def create_plant():
@@ -109,6 +143,21 @@ def create_plant_with_kwargs(title, description, **kwargs):
     }
     plant.update(kwargs)
     return plant
+
+
+def transform_plants(plant_list, transform_func):
+    return [transform_func(plant) for plant in plant_list]
+
+def plant_summary(plant):
+    return {
+        'title': plant['title'],
+        'description': plant['description']
+    }
+
+@app.route('/plants/summaries', methods=['GET'])
+def plant_summaries():
+    summaries = transform_plants(plants, plant_summary)
+    return jsonify(summaries), 200
 
 
 if __name__ == "__main__":
